19fe89b15af71231ff4e61b9853f4957e4b378dd
Alahad Patrawala <41312423+ahadpatrawala@users.noreply.github.com>
2020-07-22 17:16:15 -0700

    Updates

---
+++ precache-manifest.3d7a75aeee7b89560fb5895da8afe337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4dfb568e52623fd10fdb31c5cf65b39",
+    "url": "/ecomm-website/index.html"
+  },
+  {
+    "revision": "f1ce6d7c8fb8e6a6934e",
+    "url": "/ecomm-website/static/css/main.f637cdb1.chunk.css"
+  },
+  {
+    "revision": "2c8bf6db73dcd3a5d65b",
+    "url": "/ecomm-website/static/js/2.2b3679ef.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/ecomm-website/static/js/2.2b3679ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ce6d7c8fb8e6a6934e",
+    "url": "/ecomm-website/static/js/main.fbb92de4.chunk.js"
+  },
+  {
+    "revision": "818fb98e7b1942105d83",
+    "url": "/ecomm-website/static/js/runtime-main.2e20025a.js"
+  }
+]);
