53cb3581f889d7621ff89c6e48eafdd0533f500e
Alahad Patrawala <41312423+ahadpatrawala@users.noreply.github.com>
2020-07-24 16:06:16 -0700

    Updates

---
+++ precache-manifest.5a1fec438c5a0f08788ccbadccde86e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec9d759c9c2981de3968e74ba446e1c",
+    "url": "/github.io/ecomm-website/index.html"
+  },
+  {
+    "revision": "119d2447cbeca7ff276a",
+    "url": "/github.io/ecomm-website/static/css/main.cb5ccba4.chunk.css"
+  },
+  {
+    "revision": "6abcd67008790cce8152",
+    "url": "/github.io/ecomm-website/static/js/2.383a7b96.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/github.io/ecomm-website/static/js/2.383a7b96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "119d2447cbeca7ff276a",
+    "url": "/github.io/ecomm-website/static/js/main.bcc14c69.chunk.js"
+  },
+  {
+    "revision": "f626c5525fcc6b72c5f4",
+    "url": "/github.io/ecomm-website/static/js/runtime-main.550b068d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ecomm-website/precache-manifest.3d7a75aeee7b89560fb5895da8afe337.js"
+  "/github.io/ecomm-website/precache-manifest.5a1fec438c5a0f08788ccbadccde86e6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ecomm-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/ecomm-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
